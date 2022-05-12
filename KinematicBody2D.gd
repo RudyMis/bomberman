98ec47e0947715f22d9a6e398643708089bb3342
Cleo3927 <31996119+Cleo3927@users.noreply.github.com>
2022-05-12 16:55:48 +0200

    Add files via upload

---
+++ KinematicBody2D.gd
@@ -0,0 +1,39 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_CollisionShape2D_script_changed():
+	pass # Replace with function body.
